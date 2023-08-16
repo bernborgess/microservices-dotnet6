a49d9c1afb8cbfc053f78e936cdb7a127afc0064
bernborgess <bernborgess@outlook.com>
2023-08-16 18:31:09 -0300

    Declared product class

--- 7_Shopping/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ 7_Shopping/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -4,9 +4,10 @@ namespace GeekShopping.ProductAPI.Model.Context
 {
     public class MySQLContext : DbContext
     {
-        public MySQLContext()
-        { }
+        public MySQLContext() { }
 
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
