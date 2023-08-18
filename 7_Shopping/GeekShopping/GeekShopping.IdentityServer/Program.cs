0b9d95d516f758f71cc1a4e2209e0ce9d814a457
bernborgess <bernborgess@outlook.com>
2023-08-18 16:19:07 -0300

    Identity Server Config

--- 7_Shopping/GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ 7_Shopping/GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,42 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+    {
+        var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+        options.UseMySql(
+            connection,
+            new MySqlServerVersion(new Version(8, 0, 34)));
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var IdentityServerBuilder = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+
+})
+.AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+IdentityServerBuilder.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -13,6 +48,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
