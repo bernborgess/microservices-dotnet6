e1645cba8467756483ac98528b2e621d87d6d70b
bernborgess <bernborgess@outlook.com>
2023-08-18 17:21:20 -0300

    Config and pointing changes

--- 7_Shopping/GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ 7_Shopping/GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -6,7 +6,7 @@ namespace GeekShopping.IdentityServer.Configuration
     public static class IdentityConfiguration
     {
         public const string Admin = "Admin";
-        public const string Customer = "Customer";
+        public const string Client = "Client";
 
         public static IEnumerable<IdentityResource> IdentityResources =>
             new List<IdentityResource>
@@ -41,8 +41,8 @@ namespace GeekShopping.IdentityServer.Configuration
                     ClientId = "geek_shopping",
                     ClientSecrets = { new Secret("never_saw_such_a_bad_secrete".Sha256())},
                     AllowedGrantTypes = GrantTypes.Code,
-                    RedirectUris = { "http://localhost:52496/signin-oidc" },
-                    PostLogoutRedirectUris = { "http://localhost:52496/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:4430/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:4430/signout-callback-oidc" },
                     AllowedScopes = new List<string>
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
