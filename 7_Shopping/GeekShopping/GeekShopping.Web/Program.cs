7fb6a1ea2f2163553a78788fad537a680c31e698
bernborgess <bernborgess@outlook.com>
2023-08-21 11:45:14 -0300

    Web Authentication Configs

--- 7_Shopping/GeekShopping/GeekShopping.Web/Program.cs
+++ 7_Shopping/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,10 +1,33 @@
 using GeekShopping.Web.Services;
 using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+
+})
+.AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+.AddOpenIdConnect("oidc", options =>
+{
+    options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.ClientId = "geek_shopping";
+    options.ClientSecret = "never_saw_such_a_bad_secrete";
+    options.ResponseType = "code";
+    options.ClaimActions.MapJsonKey("role", "role", "role");
+    options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+    options.TokenValidationParameters.NameClaimType = "name";
+    options.TokenValidationParameters.RoleClaimType = "role";
+    options.Scope.Add("geek_shopping");
+    options.SaveTokens = true;
+});
+
 
 builder.Services.AddHttpClient<IProductService, ProductService>(c =>
 {
@@ -24,6 +47,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
